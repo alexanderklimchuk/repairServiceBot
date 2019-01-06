bf6686fea7c16ff6ed3f123633e5b7beb044cfa4
alexanderklimchuk <a.klimchuk@tut.by>
2019-01-06 14:14:11 +0300

    Removed unneeded code

--- src/main/java/by/losika/ts/BotApplication.java
+++ src/main/java/by/losika/ts/BotApplication.java
@@ -1,25 +1,12 @@
 package by.losika.ts;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BotApplication {
 
-  @Autowired
-  static GoogleFormSubmitter submitter;
-
-  public BotApplication(GoogleFormSubmitter submitter){
-    this.submitter = submitter;
-  }
-
   public static void main(String[] args) {
     SpringApplication.run(BotApplication.class, args);
-
-    BotApplication botApp = new BotApplication(submitter);
-
-
-
   }
 }
